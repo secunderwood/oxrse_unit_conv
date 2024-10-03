b618549a66836a097ab7a7a43a75cde0ffd00893
joshuagrier <joshua.grier@bnc.ox.ac.uk>
2024-10-03 16:12:00 +0100

    Update units.py

--- src/oxrse_unit_conv/units.py
+++ src/oxrse_unit_conv/units.py
@@ -15,8 +15,8 @@ km = kilometer
 mile = Unit(name='mile', abbr='mile', si=meter, to_si_fun=lambda n: n * 1_609.344)
 
 # meter_sq
-meter_sq = Unit(name='metre_sq', abbr='m2', si=meter, to_si_fun=lambda n: n ** 0.5)
-m2 = meter_sq
+kilometer_sq = Unit(name='kilometer_sq', abbr='km2', si=meter_sq, to_si_fun=lambda n: n * 1000000)
+km2 = kilometer_sq
 # meter_cu
 
 # kilogram
