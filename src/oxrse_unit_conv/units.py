730cd98239d89573499167191657d6f66b4c8d96
joshuagrier <joshua.grier@bnc.ox.ac.uk>
2024-10-03 16:04:02 +0100

    Update units.py
    Fixed the abbreviation of meter_sq

--- src/oxrse_unit_conv/units.py
+++ src/oxrse_unit_conv/units.py
@@ -15,8 +15,8 @@ km = kilometer
 mile = Unit(name='mile', abbr='mile', si=meter, to_si_fun=lambda n: n * 1_609.344)
 
 # meter_sq
-meter_sq = Unit(name='metre_sq', abbr='m^2', si=meter, to_si_fun=lambda n: n ** 0.5)
-
+meter_sq = Unit(name='metre_sq', abbr='m2', si=meter, to_si_fun=lambda n: n ** 0.5)
+m2 = meter_sq
 # meter_cu
 
 # kilogram
